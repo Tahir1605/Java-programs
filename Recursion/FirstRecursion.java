f739e3167b04054402914adfb92e882f8af6da12
Tahirul Islam <5566tahirul@gmail.com>
2025-04-03 15:00:33 +0530

    print numbers using recursion

--- Recursion/FirstRecursion.java
+++ Recursion/FirstRecursion.java
@@ -1,31 +1,13 @@
-import java.util.Scanner;
-class FirstRecursion
-  {
-     public static long fact(int n)
-        {
-           if(n == 0 || n == 1)
-             {
-                return 1;
-             }
-           else
-             {
-                return n*fact(n-1);
-             }
-        }
-     public static void main(String[] args)
-        {
-           Scanner obj = new Scanner(System.in);
-           System.out.println("Enter a number : ");
-           int num = obj.nextInt();
-           if(num < 0)
-             {
-                System.out.println("invalid number");
-             }
-           else
-             {
-               System.out.println("Factorial of " + num + " is: " + fact(num));
-             }
-             obj.close();
-            
-        }
-  }
+class FirstRecursion{
+   public static int printNumber(int n){
+      if(n>5){
+         return 0;
+      }
+      System.out.println(n);
+      return printNumber(n+1);
+   }
+   public static void main(String[] args){
+      int n = 1;
+      printNumber(n);
+   }
+}
